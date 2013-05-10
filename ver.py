49cb5005d81d7f11cb25133316418cb46be157a6
Wen YE <whusnoopy@gmail.com>
2013-05-10 11:10:27 +0800

    skip login when already online

--- ver.py
+++ ver.py
@@ -12,27 +12,32 @@ def login():
 
   encode_pas = urllib.quote(base64.b64encode(password))
 
-  login_para = { \
-    "username": username, \
-    "password": password, \
-    "url": "", \
-    "password_enc": encode_pas, \
-    "newpassword_enc": "", \
-    "retype_newpassword_enc": "", \
-    "login": "1", \
-    "login_type": "login", \
-    "uri": "aHR0cDovLzE5Mi4xNjguNjAuNjUv", \
-    "password_type": "normal", \
+  login_para = {
+    "username": username,
+    "password": password,
+    "url": "",
+    "password_enc": encode_pas,
+    "newpassword_enc": "",
+    "retype_newpassword_enc": "",
+    "login": "1",
+    "login_type": "login",
+    "uri": "aHR0cDovLzE5Mi4xNjguNjAuNjUv",
+    "password_type": "normal",
     "password_orig": ""}
 
   login_url = "http://10.2.255.254"
   login_data = urllib.urlencode(login_para)
 
-  f = urllib2.urlopen(url=login_url, data=login_data)
+  try:
+    f = urllib2.urlopen(url=login_url, data=login_data)
+  except:
+    print 'already online'
+    return False
 
   print f.read()
 
-  return
+  return True
+
 
 def logout():
   logout_url = "http://10.2.255.254:81"
@@ -44,8 +49,10 @@ def logout():
 
   return
 
+
 if __name__ == "__main__":
   if len(sys.argv) <= 1 or sys.argv[1].lower() != "logout":
     login()
   else:
     logout()
+
